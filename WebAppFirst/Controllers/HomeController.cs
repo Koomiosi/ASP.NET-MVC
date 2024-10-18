d09d5f034cecfa0fea0e8d053b69e32e49ce2f0b
Tony Kelanen <tony.kelanen@student.careeria.fi>
2024-10-18 10:25:16 +0300

    Add project files.

---
+++ WebAppFirst/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+
+namespace WebAppFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Yhtiön perustietojen kuvailua";
+            ViewBag.Herja = "Ole huolellinen, niin ei tule virhettä ";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Yhteystietomme.";
+
+            return View();
+        }
+
+        public ActionResult Map()
+        {
+            ViewBag.Message = "Saapumisohje";
+
+            return View();
+        }
+    }
+}
---
+++ WebAppFirst/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WebAppFirst.Models;
+
+namespace WebAppFirst.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            NorthwindOriginalEntities3 db = new NorthwindOriginalEntities3();
+            List<Products> model = db.Products.ToList();
+            db.Dispose();
+            return View(model);
+        }
+
+        public ActionResult Index2()
+        {
+            NorthwindOriginalEntities3 db = new NorthwindOriginalEntities3();
+            List<Products> model = db.Products.ToList();
+            db.Dispose();
+            return View(model);
+        }
+    }
+}
