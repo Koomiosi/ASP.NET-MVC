f064f729bf16892e7a4b357c96165ddaa6029985
Tony Kelanen <tony.kelanen@student.careeria.fi>
2024-10-20 23:23:58 +0300

    Order view edit and ne icons

--- WebAppFirst/Controllers/ProductsController.cs
+++ WebAppFirst/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 using WebAppFirst.Models;
 
@@ -7,34 +8,125 @@ namespace WebAppFirst.Controllers
 {
     public class ProductsController : Controller
     {
+        private NorthwindOriginalEntities3 db = new NorthwindOriginalEntities3();
+
         // GET: Products
         public ActionResult Index()
         {
-            if (Session["UserName"] == null)
-            {
-                return RedirectToAction("Login", "Home");
-            }
-            else
-            {
-                if (Session["UserName"] == null)
-                {
-                    return RedirectToAction("Login", "Home");
-                }
-                else ViewBag.LoggedStatus = "In";
-                NorthwindOriginalEntities3 db = new NorthwindOriginalEntities3();
-                List<Products> model = db.Products.ToList();
-                db.Dispose();
-                return View(model);
-            }
-
+            var products = db.Products.Include(p => p.Categories).Include(p => p.Suppliers);
+            return View(products.ToList());
         }
 
-        public ActionResult Index2()
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
         {
-            NorthwindOriginalEntities3 db = new NorthwindOriginalEntities3();
-            List<Products> model = db.Products.ToList();
-            db.Dispose();
-            return View(model);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(products);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued,ImageLink")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(products);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", products.SupplierID);
+            return View(products);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", products.SupplierID);
+            return View(products);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued,ImageLink")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(products).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", products.SupplierID);
+            return View(products);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(products);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Products products = db.Products.Find(id);
+            db.Products.Remove(products);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ WebAppFirst/Controllers/ProductsController2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WebAppFirst.Models;
+
+namespace WebAppFirst.Controllers
+{
+    public class ProductsController2 : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            else
+            {
+                if (Session["UserName"] == null)
+                {
+                    return RedirectToAction("Login", "Home");
+                }
+                else ViewBag.LoggedStatus = "In";
+                NorthwindOriginalEntities3 db = new NorthwindOriginalEntities3();
+                List<Products> model = db.Products.ToList();
+                db.Dispose();
+                return View(model);
+            }
+
+        }
+
+        public ActionResult Index2()
+        {
+            NorthwindOriginalEntities3 db = new NorthwindOriginalEntities3();
+            List<Products> model = db.Products.ToList();
+            db.Dispose();
+            return View(model);
+        }
+    }
+}
